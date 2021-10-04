ad48afaabc993da811d66f5369c71d33a5301472
Marat <drof002665@gmail.com>
2021-10-05 01:40:39 +0400

    Added ecoytem config for pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,9 +2,10 @@ module.exports = {
   apps : [{
     name: 'from-config',
     script: 'server/server.js',
-    watch: '.',
     ignore_watch: 'node_modules',
     wait_ready: true,
-    max_memory_restart: '15G'
+    max_memory_restart: '15G',
+    "autorestart" : false,
+    "node_args":["--max-old-space-size=18392"]
   }],
 };
